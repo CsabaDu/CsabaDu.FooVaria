8ed6861d19465f1f89aebaad48886ea6a91452fb
DudasCsaba <dudas.csaba@kenderetail.hu>
2023-08-29 16:51:36 +0200

    Measurables cont.

--- Measurables/Behaviors/IVolumetricWeight.cs
+++ Measurables/Behaviors/IVolumetricWeight.cs
@@ -2,5 +2,5 @@
 
 public interface IVolumetricWeight<out T> where T : class, IMeasure
 {
-    T GetVolumetricWeight(IVolume volume, decimal ratio, bool isGreater = true, WeightUnit? weightUnit = null);
+    T GetVolumetricWeight(IVolume volume, decimal ratio, bool isGreater = true);
 }
