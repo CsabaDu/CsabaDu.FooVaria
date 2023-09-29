8e55c865df1220edba22e68aa8e9ffbef219ce02
DudasCsaba <dudas.csaba@kenderetail.hu>
2023-09-29 22:22:16 +0200

    Class diagrams

--- Measurables/Types/IBaseMeasure.cs
+++ Measurables/Types/IBaseMeasure.cs
@@ -6,7 +6,14 @@ public interface IBaseMeasure : IMeasurable, IQuantifiable, IRateComponent<IBase
     decimal DefaultQuantity { get; }
 
     IBaseMeasure GetBaseMeasure(ValueType quantity, Enum measureUnit);
-    IBaseMeasure GetBaseMeasure(IMeasurable other);
-    LimitMode? GetLimitMode();
+    IBaseMeasure GetBaseMeasure(IBaseMeasure other);
+    IBaseMeasure GetBaseMeasure(IBaseMeasureFactory baseMeasureFactory, IBaseMeasure baseMeasure);
+
     bool TryGetBaseMeasure(ValueType quantity, Enum measureUnit, decimal exchangeRate, string customName, [NotNullWhen(true)] out IBaseMeasure? baseMeasure);
+    LimitMode? GetLimitMode();
 }
+
+    //RateComponentCode GetRateComponentCode(IBaseMeasure baseMeasure);
+    //IBaseMeasure? GetBaseMeasure(ValueType quantity, string name);
+    //IBaseMeasure GetBaseMeasure(ValueType quantity, string customName, MeasureUnitTypeCode measureUnitTypeCode, decimal exchangeRate);
+    //IBaseMeasure GetBaseMeasure(ValueType quantity, IMeasurement measurement);
