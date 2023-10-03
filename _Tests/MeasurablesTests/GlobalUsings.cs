fcfc0693ac85d0eda94dc11acd3e131dd21a1f7d
DudasCsaba <dudas.csaba@kenderetail.hu>
2023-10-03 11:57:29 +0200

    MeasurablesTests.csproj added

---
+++ _Tests/MeasurablesTests/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using CsabaDu.FooVaria.Common.Enums;
+global using CsabaDu.FooVaria.Common.Types;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using System.ComponentModel;
---
+++ _Tests/MeasurablesTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CsabaDu.FooVaria.Tests.MeasurablesTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
