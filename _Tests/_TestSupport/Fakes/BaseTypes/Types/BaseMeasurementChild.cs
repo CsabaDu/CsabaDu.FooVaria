a1f62d2af9213f03edfb0f913bcda337239e5bf3
DudasCsaba <dudas.csaba@kenderetail.hu>
2024-02-13 19:21:59 +0100

    minor

--- _Tests/_TestSupport/Fakes/BaseTypes/Types/BaseMeasurementChild.cs
+++ _Tests/_TestSupport/Fakes/BaseTypes/Types/BaseMeasurementChild.cs
@@ -2,11 +2,6 @@
 
 internal sealed class BaseMeasurementChild(IBaseMeasurementFactory factory, Enum measureUnit) : BaseMeasurement(factory, measureUnit)
 {
-    public override decimal GetExchangeRate()
-    {
-        throw new NotImplementedException();
-    }
-
     public override decimal GetExchangeRate(string name)
     {
         throw new NotImplementedException();
