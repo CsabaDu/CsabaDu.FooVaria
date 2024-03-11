9e84fd7557cc3077c79f64a372d76d23587a36ab
DudasCsaba <dudas.csaba@kenderetail.hu>
2024-03-11 09:27:56 +0100

    Tetst ubdated with MeasurableHelpers

--- _Tests/UnitTests/BaseTypes/BaseMeasurementsTests/GlobalUsings.cs
+++ _Tests/UnitTests/BaseTypes/BaseMeasurementsTests/GlobalUsings.cs
@@ -1,7 +1,6 @@
-global using CsabaDu.FooVaria.BaseTypes.BaseMeasurements.Types;
 global using CsabaDu.FooVaria.BaseTypes.Measurables.Enums;
 global using CsabaDu.FooVaria.Tests.TestSupport.Fakes.BaseTypes.Factories;
 global using CsabaDu.FooVaria.Tests.TestSupport.Fakes.BaseTypes.Types;
 global using CsabaDu.FooVaria.Tests.TestSupport.Params;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using static CsabaDu.FooVaria.BaseTypes.Measurables.Types.Implementations.Measurable;
+global using static CsabaDu.FooVaria.BaseTypes.Measurables.Statics.MeasurableHelpers;
--- _Tests/UnitTests/BaseTypes/MeasurablesTests/GlobalUsings.cs
+++ _Tests/UnitTests/BaseTypes/MeasurablesTests/GlobalUsings.cs
@@ -1,11 +1,8 @@
 global using CsabaDu.FooVaria.BaseTypes.Common;
 global using CsabaDu.FooVaria.BaseTypes.Measurables.Enums;
-global using CsabaDu.FooVaria.BaseTypes.Measurables.Factories;
 global using CsabaDu.FooVaria.BaseTypes.Measurables.Statics;
-global using CsabaDu.FooVaria.BaseTypes.Measurables.Types;
-global using CsabaDu.FooVaria.Tests.TestSupport.Fakes.BaseTypes.Factories;
 global using CsabaDu.FooVaria.Tests.TestSupport.Fakes.BaseTypes.Types;
 global using CsabaDu.FooVaria.Tests.TestSupport.Params;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using System.ComponentModel;
-global using static CsabaDu.FooVaria.BaseTypes.Measurables.Types.Implementations.Measurable;
+global using static CsabaDu.FooVaria.BaseTypes.Measurables.Statics.MeasurableHelpers;
