221b2dd3928eb417b5138931a2930d452cd41cf7
DudasCsaba <dudas.csaba@kenderetail.hu>
2024-03-14 12:34:01 +0100

    delete BaseProportions

--- _BaseTypes/BaseProportions/Factories/ISimpleRateFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using CsabaDu.FooVaria.AbstractTypes.SimpleRates.Types;
-
-namespace CsabaDu.FooVaria.AbstractTypes.SimpleRates.Factories
-{
-    public interface ISimpleRateFactory : IBaseRateFactory/*, IFactory<IBaseRate>*/
-    {
-        ISimpleRate CreateSimpleRate(MeasureUnitCode numeratorCode, decimal defaultQuantity, MeasureUnitCode denominatorCode);
-    }
-}
