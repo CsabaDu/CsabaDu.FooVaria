2b08b36e80732f245d5d5a08e13eb33a5481d496
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2024-12-27 13:44:03 +0100

    BulkSpreads comments

--- BulkSpreads/Types/Implementations/BulkBody.cs
+++ BulkSpreads/Types/Implementations/BulkBody.cs
@@ -1,16 +1,22 @@
-﻿using CsabaDu.FooVaria.BaseTypes.Quantifiables.Types;
-using System.Diagnostics.CodeAnalysis;
-
-namespace CsabaDu.FooVaria.BulkSpreads.Types.Implementations;
+﻿namespace CsabaDu.FooVaria.BulkSpreads.Types.Implementations;
 
 internal sealed class BulkBody : BulkSpread<IBulkBody, IVolume, VolumeUnit>, IBulkBody
 {
     #region Constructors
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BulkBody"/> class by copying another <see cref="IBulkBody"/> instance.
+    /// </summary>
+    /// <param name="other">The other <see cref="IBulkBody"/> instance to copy.</param>
     internal BulkBody(IBulkBody other) : base(other)
     {
         Factory = other.Factory;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BulkBody"/> class with the specified factory and volume.
+    /// </summary>
+    /// <param name="factory">The factory used to create the <see cref="BulkBody"/>.</param>
+    /// <param name="volume">The volume to use.</param>
     internal BulkBody(IBulkBodyFactory factory, IVolume volume) : base(factory, volume)
     {
         Factory = factory;
@@ -18,31 +24,61 @@ internal sealed class BulkBody : BulkSpread<IBulkBody, IVolume, VolumeUnit>, IBu
     #endregion
 
     #region Properties
+    /// <summary>
+    /// Gets the factory associated with the <see cref="BulkBody"/>.
+    /// </summary>
     public IBulkBodyFactory Factory { get; init; }
     #endregion
 
     #region Public methods
+    /// <summary>
+    /// Gets a new <see cref="IBody"/> instance.
+    /// </summary>
+    /// <returns>A new <see cref="IBody"/> instance.</returns>
     public IBody GetBody()
     {
         return GetNew(this);
     }
 
+    /// <summary>
+    /// Gets a new <see cref="IBulkBody"/> instance based on the specified radius and height.
+    /// </summary>
+    /// <param name="radius">The radius to use.</param>
+    /// <param name="height">The height to use.</param>
+    /// <returns>A new <see cref="IBulkBody"/> instance.</returns>
     public IBulkBody GetBulkBody(IExtent radius, IExtent height)
     {
         return GetBulkSpread(radius, height);
     }
 
+    /// <summary>
+    /// Gets a new <see cref="IBulkBody"/> instance based on the specified length, width, and height.
+    /// </summary>
+    /// <param name="length">The length to use.</param>
+    /// <param name="width">The width to use.</param>
+    /// <param name="height">The height to use.</param>
+    /// <returns>A new <see cref="IBulkBody"/> instance.</returns>
     public IBulkBody GetBulkBody(IExtent length, IExtent width, IExtent height)
     {
         return GetBulkSpread(length, width, height);
     }
 
     #region Override methods
+    /// <summary>
+    /// Gets the factory associated with the <see cref="BulkBody"/>.
+    /// </summary>
+    /// <returns>The factory associated with the <see cref="BulkBody"/>.</returns>
     public override IBulkBodyFactory GetFactory()
     {
         return Factory;
     }
 
+    /// <summary>
+    /// Gets a new <see cref="IBulkBody"/> instance based on the specified spread.
+    /// </summary>
+    /// <param name="baseSppread">The spread to use.</param>
+    /// <returns>A new <see cref="IBulkBody"/> instance.</returns>
+    /// <exception cref="ArgumentTypeOutOfRangeException">Thrown if the spread is not valid.</exception>
     public override IBulkBody GetBulkSpread(ISpread baseSppread)
     {
         if (NullChecked(baseSppread, nameof(baseSppread)) is IBody body) return GetBulkSpread(body.GetSpreadMeasure());
--- BulkSpreads/Types/Implementations/BulkSurface.cs
+++ BulkSpreads/Types/Implementations/BulkSurface.cs
@@ -1,16 +1,25 @@
-﻿using CsabaDu.FooVaria.BaseTypes.Quantifiables.Types;
-using System.Diagnostics.CodeAnalysis;
+﻿namespace CsabaDu.FooVaria.BulkSpreads.Types.Implementations;
 
-namespace CsabaDu.FooVaria.BulkSpreads.Types.Implementations;
-
+/// <summary>
+/// Represents a bulk surface implementation.
+/// </summary>
 internal sealed class BulkSurface : BulkSpread<IBulkSurface, IArea, AreaUnit>, IBulkSurface
 {
     #region Constructors
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BulkSurface"/> class with the specified other bulk surface.
+    /// </summary>
+    /// <param name="other">The other bulk surface to copy.</param>
     internal BulkSurface(IBulkSurface other) : base(other)
     {
         Factory = other.Factory;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BulkSurface"/> class with the specified factory and area.
+    /// </summary>
+    /// <param name="factory">The factory used to create the bulk surface.</param>
+    /// <param name="area">The area to use.</param>
     internal BulkSurface(IBulkSurfaceFactory factory, IArea area) : base(factory, area)
     {
         Factory = factory;
@@ -18,31 +27,59 @@ internal sealed class BulkSurface : BulkSpread<IBulkSurface, IArea, AreaUnit>, I
     #endregion
 
     #region Properties
+    /// <summary>
+    /// Gets the factory used to create the bulk surface.
+    /// </summary>
     public IBulkSurfaceFactory Factory { get; init; }
     #endregion
 
     #region Public methods
+    /// <summary>
+    /// Gets a new bulk surface based on the specified radius.
+    /// </summary>
+    /// <param name="radius">The radius to use.</param>
+    /// <returns>The new bulk surface.</returns>
     public IBulkSurface GetBulkSurface(IExtent radius)
     {
         return GetBulkSpread(radius);
     }
 
+    /// <summary>
+    /// Gets a new bulk surface based on the specified length and width.
+    /// </summary>
+    /// <param name="length">The length to use.</param>
+    /// <param name="width">The width to use.</param>
+    /// <returns>The new bulk surface.</returns>
     public IBulkSurface GetBulkSurface(IExtent length, IExtent width)
     {
         return GetBulkSpread(length, width);
     }
 
+    /// <summary>
+    /// Gets a new surface based on the current bulk surface.
+    /// </summary>
+    /// <returns>The new surface.</returns>
     public ISurface GetSurface()
     {
         return GetNew(this);
     }
 
     #region Override methods
+    /// <summary>
+    /// Gets the factory used to create the bulk surface.
+    /// </summary>
+    /// <returns>The factory.</returns>
     public override IBulkSurfaceFactory GetFactory()
     {
         return Factory;
     }
 
+    /// <summary>
+    /// Gets a new bulk surface based on the specified base spread.
+    /// </summary>
+    /// <param name="baseSppread">The base spread to use.</param>
+    /// <returns>The new bulk surface.</returns>
+    /// <exception cref="ArgumentTypeOutOfRangeException">Thrown if the base spread is not valid.</exception>
     public override IBulkSurface GetBulkSpread(ISpread baseSppread)
     {
         if (NullChecked(baseSppread, nameof(baseSppread)) is ISurface surface) return GetBulkSpread(surface.GetSpreadMeasure());
